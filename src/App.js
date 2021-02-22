cfcd8f2cf6ccf5e23587f43c2d9c0984da20c7c6
mlgmontage <kettleofrassel1@gmail.com>
2021-02-22 22:16:01 +0500

    navigation component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Splash from "./components/splash";
+import Navigation from "./components/navigation";
 
 const App = () => (
   <div className="container">
+    <Navigation />
     <Splash />
   </div>
 );
---
+++ src/components/navigation.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Navigation = () => (
+  <div className="row navigation">
+    <div className="four columns">
+      <a className="logo" href="#">
+        <span className="fa fa-paperclip" style={{ fontSize: "40px" }}></span>
+        <span style={{ fontSize: "24px", fontWeight: "bold" }}>
+          WordClipper
+        </span>
+      </a>
+    </div>
+
+    <div className="two columns">
+      <div className="fa fa-search nav-icon"></div>
+      <a href="#" className="nav-text">
+        Explore
+      </a>
+    </div>
+
+    <div className="two columns">
+      <div className="fa fa-university nav-icon"></div>
+      <a href="./train.html" className="nav-text">
+        Train
+      </a>
+    </div>
+    <div className="two columns">
+      <div className="fa fa-bar-chart nav-icon"></div>
+      <a href="#" className="nav-text">
+        Stats
+      </a>
+    </div>
+    <div className="two columns">
+      <div className="fa fa-archive nav-icon"></div>
+      <a href="./archive.html" className="nav-text">
+        Archive
+      </a>
+    </div>
+  </div>
+);
+
+export default Navigation;

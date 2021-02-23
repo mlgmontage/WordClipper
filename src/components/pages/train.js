bacc192816535ab1f6282fa609de847ed6c06a7f
mlgmontage <kettleofrassel1@gmail.com>
2021-02-23 22:22:22 +0500

    babel plugin class properties

--- src/components/pages/train.js
+++ src/components/pages/train.js
@@ -1,32 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
+import data from "../../data/data";
 
-const Train = () => (
-  <div>
-    <div className="row square">
-      <div className="six columns">
-        <h2 id="train_word">abundant</h2>
-      </div>
-      <div className="six columns" style={{ fontSize: "18px" }}>
-        present in great quantity; more than adequate: an abundant supply of
-        water.
-      </div>
-    </div>
-    <div className="row controls" style={{ fontSize: "48px" }}>
-      <div className="five columns">
-        <span className="fa fa-arrow-left"></span>
-      </div>
+class Train extends Component {
+  state = {
+    index: 0,
+    data,
+  };
+  render() {
+    return (
+      <div>
+        <div className="row square">
+          <div className="six columns">
+            <h2>{this.state.data[this.state.index].word}</h2>
+          </div>
+          <div className="six columns" style={{ fontSize: "18px" }}>
+            {this.state.data[this.state.index].description}
+          </div>
+        </div>
+        <div className="row controls" style={{ fontSize: "48px" }}>
+          <div className="five columns">
+            <span className="fa fa-arrow-left"></span>
+          </div>
 
-      <div className="two columns" style={{ textAlign: "center" }}>
-        <span className="fa fa-check"></span>
-        <br />
-        <a className="fa fa-plus" href="./create.html"></a>
-      </div>
+          <div className="two columns" style={{ textAlign: "center" }}>
+            <span className="fa fa-check"></span>
+            <br />
+            <a className="fa fa-plus" href="./create.html"></a>
+          </div>
 
-      <div className="five columns" style={{ textAlign: "right" }}>
-        <span className="fa fa-arrow-right"></span>
+          <div className="five columns" style={{ textAlign: "right" }}>
+            <span className="fa fa-arrow-right"></span>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default Train;

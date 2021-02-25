73bbd8cad97454834fdd1b72fd711e6fb2379db8
mlgmontage <kettleofrassel1@gmail.com>
2021-02-25 22:18:36 +0500

    archive page

--- src/components/pages/archive.js
+++ src/components/pages/archive.js
@@ -1,8 +1,38 @@
 import React, { Component } from "react";
+import host from "../../host";
 
 class Archive extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  state = {
+    data: [{ _id: "", word: "", description: "" }],
+  };
+
+  componentDidMount() {
+    fetch(`${host}/api/words/archive`)
+      .then((response) => response.json())
+      .then((json) => {
+        if (json) {
+          this.setState({ data: json });
+        }
+      });
+  }
+
   render() {
-    return <div>Archive</div>;
+    return (
+      <div>
+        <h2>Archive</h2>
+        {this.state.data.map((item) => (
+          <div key={item._id}>
+            <h4>{item.word}</h4>
+            <div>{item.description}</div>
+            <hr />
+          </div>
+        ))}
+      </div>
+    );
   }
 }
 
